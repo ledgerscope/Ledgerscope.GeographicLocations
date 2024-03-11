3e6337e2d61e70f9bfc43aed193eadc6c6eda3a9
Steve Aze <steve@ledgerscope.com>
2024-03-11 13:52:41 +0000

    Comments and readme.

--- Ledgerscope.GeographicLocations/GeoLocation.cs
+++ Ledgerscope.GeographicLocations/GeoLocation.cs
@@ -2,6 +2,10 @@
 
 namespace Ledgerscope.GeographicLocations
 {
+	/// <summary>
+	/// Ledgerscope snapshot in time of Windows geographic location data.
+	/// </summary>
+	/// <remarks>Done so that our Linux build servers have the Windows location data too.</remarks>
 	public partial class GeoLocation
 	{
 		public string Nation { get; }
@@ -15,26 +19,26 @@ namespace Ledgerscope.GeographicLocations
 		public string OfficialName { get; }
 		public string TimeZones { get; }
 		public string OfficialLanguages { get; }
-        public string Currency { get; set; }
+		public string Currency { get; set; }
 
-        public GeoLocation(string nation, string latitude, string longitude, string iso2, string iso3, string rfc1766, string lcid, 
-            string friendlyName, string officialName, string timeZones, string officialLanguages, string currency)
-        {
-            Nation = nation;
-            Latitude = latitude;
-            Longitude = longitude;
-            ISO2 = iso2;
-            ISO3 = iso3;
-            Rfc1766 = rfc1766;
-            Lcid = lcid;
-            FriendlyName = friendlyName;
-            OfficialName = officialName;
-            TimeZones = timeZones;
-            OfficialLanguages = officialLanguages;
-            Currency = currency;
-        }
+		public GeoLocation(string nation, string latitude, string longitude, string iso2, string iso3, string rfc1766, string lcid, 
+			string friendlyName, string officialName, string timeZones, string officialLanguages, string currency)
+		{
+			Nation = nation;
+			Latitude = latitude;
+			Longitude = longitude;
+			ISO2 = iso2;
+			ISO3 = iso3;
+			Rfc1766 = rfc1766;
+			Lcid = lcid;
+			FriendlyName = friendlyName;
+			OfficialName = officialName;
+			TimeZones = timeZones;
+			OfficialLanguages = officialLanguages;
+			Currency = currency;
+		}
 
-        public override string ToString()
+		public override string ToString()
 		{
 			return $"{FriendlyName} [{ISO2}]";
 		}
---
+++ readme.md
@@ -0,0 +1,12 @@
+﻿# Ledgerscope.GeographicLocations
+
+A library for getting at a snapshot in time of the geo locations info in Windows, and making it available to our dev machines build servers (including Linux ones).  
+
+## Location
+
+
+`ledgerscope/Utilities/Repos/Files/Ledgerscope.GeographicLocations`
+
+## Build and deploy
+
+The pipelines task builds it into a nuget package and deploys it to our nuget feed (LSFeed).
