f7a6185891ccff34ec7ed8876dbc115606788821
Steve Aze <steve@ledgerscope.com>
2024-03-12 13:06:26 +0000

    Add a compilation check against codegen silent failures.

---
+++ Ledgerscope.GeographicLocations/ProblemCatcher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ledgerscope.GeographicLocations
+{
+	internal class ProblemCatcher
+	{
+		public static void TestForProblems()
+		{
+			// If you are finding that this will not compile, it'll be because the 
+			// code generators aren't running, or are (silently) throwing errors when they run.
+
+			_ = GeoLocation.GetAll();
+			_ = WindowsCountry.GetAll();
+		}
+	}
+}
